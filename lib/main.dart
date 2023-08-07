50a46c5c19ad708763b6e5ec02de841cc3e90ff0
Akniyet <akniyetbegimkulov@gmail.com>
2023-08-07 14:55:54 +0600

    initial commit

---
+++ lib/main.dart
@@ -0,0 +1,41 @@
+import 'package:flutter/material.dart';
+import 'package:themoviedb/Theme/app_colors.dart';
+import 'package:themoviedb/widgets/auth/auth_widget.dart';
+import 'package:themoviedb/widgets/main_screen/main_screen_widget.dart';
+import 'package:themoviedb/widgets/movie_details/movie_details_widget.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        appBarTheme: AppBarTheme(
+          backgroundColor: AppColors.mainDarkBlue,
+        ),
+        bottomNavigationBarTheme: BottomNavigationBarThemeData(
+          backgroundColor: AppColors.mainDarkBlue,
+          selectedItemColor: Colors.white,
+          unselectedItemColor: Colors.grey,
+        ),
+      ),
+      routes: {
+        '/auth': (context) => AuthWidget(),
+        '/main_screen': (context) => MainScreenWidget(),
+        '/main_screen/movie_details': (context) {
+          final arguments = ModalRoute.of(context)?.settings.arguments;
+          if (arguments is int) {
+            return MovieDetailsWidget(movieId: arguments);
+          } else {
+            return MovieDetailsWidget(movieId: 0);
+          }
+        },
+      },
+      initialRoute: '/auth',
+    );
+  }
+}
---
+++ lib/resources/app_images.dart
@@ -0,0 +1,12 @@
+part of 'resources.dart';
+
+class AppImages {
+  AppImages._();
+
+  static const String actor = 'images/actor.jpeg';
+  static const String contrebuter = 'images/contrebuter.jpeg';
+  static const String moviePlacholder = 'images/movie_placholder.jpeg';
+  static const String recomendation = 'images/recomendation.jpeg';
+  static const String topHeaderSubImage = 'images/top_header_sub_image.jpeg';
+  static const String topHeader = 'images/top_header.jpeg';
+}
